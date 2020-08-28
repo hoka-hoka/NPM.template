82fc7bf7c83b1d7a135d2e5933414be4dcfb77f2
semyonix_pokemonix <sff1@tpu.ru>
2020-08-28 15:23:02 +0700

    update package.json

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 (function ($) {
-    let a = $('<div class="newElement">');
+  let a = $('<div class="newElement">');
 })(jQuery);
-function f({ a, b }) {
-    return a;
+
+function f({
+  a,
+  b
+}) {
+  return a;
 }
-let rezult = f({ a: 'Vasia', b: 10 });
+
+let rezult = f({
+  a: 'Vasia',
+  b: 10
+});
 console.log(rezult);
-export {};
-//# sourceMappingURL=script.js.map
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3RzL3NjcmlwdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFFQSxDQUFDLFVBQVMsQ0FBVCxFQUFVO0FBQ1QsTUFBSSxDQUFDLEdBQXdCLENBQUMsQ0FBQywwQkFBRCxDQUE5QjtBQUNELENBRkQsRUFFRyxNQUZIOztBQU1BLFNBQVMsQ0FBVCxDQUFXO0FBQUMsRUFBQSxDQUFEO0FBQUksRUFBQTtBQUFKLENBQVgsRUFBb0I7QUFDbEIsU0FBTyxDQUFQO0FBQ0Q7O0FBQ0QsSUFBSSxNQUFNLEdBQUcsQ0FBQyxDQUFDO0FBQUMsRUFBQSxDQUFDLEVBQUMsT0FBSDtBQUFZLEVBQUEsQ0FBQyxFQUFDO0FBQWQsQ0FBRCxDQUFkO0FBQ0EsT0FBTyxDQUFDLEdBQVIsQ0FBWSxNQUFaIiwic291cmNlUm9vdCI6IiJ9
