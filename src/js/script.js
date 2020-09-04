1b04671c182d85332d19301c323d4f38cba90da5
semyonix_pokemonix <sff1@tpu.ru>
2020-09-04 20:12:12 +0600

    update watch

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,4 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-(function ($) {
-  let a = $('<div class="newElement">');
-})(jQuery);
-
-function f({
-  a,
-  b
-}) {
-  return a;
-}
-
-let rezult = f({
-  a: 'Vasia',
-  b: 10
-});
-console.log(rezult);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3RzL3NjcmlwdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFFQSxDQUFDLFVBQVMsQ0FBVCxFQUFVO0FBQ1QsTUFBSSxDQUFDLEdBQXdCLENBQUMsQ0FBQywwQkFBRCxDQUE5QjtBQUNELENBRkQsRUFFRyxNQUZIOztBQU1BLFNBQVMsQ0FBVCxDQUFXO0FBQUMsRUFBQSxDQUFEO0FBQUksRUFBQTtBQUFKLENBQVgsRUFBb0I7QUFDbEIsU0FBTyxDQUFQO0FBQ0Q7O0FBQ0QsSUFBSSxNQUFNLEdBQUcsQ0FBQyxDQUFDO0FBQUMsRUFBQSxDQUFDLEVBQUMsT0FBSDtBQUFZLEVBQUEsQ0FBQyxFQUFDO0FBQWQsQ0FBRCxDQUFkO0FBQ0EsT0FBTyxDQUFDLEdBQVIsQ0FBWSxNQUFaIiwic291cmNlUm9vdCI6IiJ9
+let a = 'Vasia';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3RzL3NjcmlwdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7QUFDQSxJQUFJLENBQUMsR0FBVyxPQUFoQiIsInNvdXJjZVJvb3QiOiIifQ==
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,13 +1,28 @@
-import $ from 'jquery';
-
-(function($) {
-  let a: JQuery<HTMLElement> = $('<div class="newElement">');
-})(jQuery);
-
-type C = {a: string, b?: number};
-
-function f({a, b}: C): string {
-  return a;
-}
-let rezult = f({a:'Vasia', b:10});
-console.log(rezult);
+"use strict";
+let a: string = 'Vasia';
+//console.log(a);
+// class ObserverList {
+//   constructor() {
+//     this.observerLsit = [];
+//   }
+//   add(obj) {
+//     this.observerLsit.push(obj);
+//   }
+//   get(index) {
+//     if ( index > -1 && index < this.observerLsit.length ) {
+//       return this.observerLsit[index];
+//     }
+//   }
+//   indexof(obj, startIndex) {
+//     var i = startIndex;
+//     while ( i < this.observerLsit.length ) {
+//       if ( this.observerLsit[i] === obj ) {
+//         return;
+//       }
+//       i++;
+//     }
+//   }
+//   removeAt(index) {
+//     this.observerLsit.splice(index, 1);
+//   }
+// }
